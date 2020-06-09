2ad11def9c5b33f1cb2bbd424f55b6f5a2de6053
amj311 <amjudd315@gmail.com>
2020-06-09 09:08:15 -0600

    modified calendar.printmonths to generate chart. calc1yr prints months and has a slowdown loop for visual effect

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 const moment = require('moment');
 const bstSrc = require('./BST'); 
 
-console.log('hello world.')
-
 var accounts = {
 
     26739094: {
@@ -45,13 +43,21 @@ var calendar = {
     monthSumsBST: new bstSrc.BST(),
     
     printMonths() {
+
+        let minTs = this.monthSumsBST.minValue();
+        let maxTs = this.monthSumsBST.maxValue();
+        
+        console.log('\n          Projected Income Over Period ' + this.monthSums[minTs].month.format('MMM YYYY') + " - " + this.monthSums[maxTs].month.format('MMM YYYY'))
+
         const colWidth = 10;
         var headerStr = "";
         var dividerStr = "";
         var balanceRowStr = "";
+        var plotData = [];
 
         for (let [key, summary] of Object.entries(this.monthSums)) {
-            console.log(key, summary)
+            plotData.push(summary.totalAssets)
+
             var headerGap = colWidth - summary.month.format('MMM').length;
             headerStr += summary.month.format('MMM');
             for (i = 0; i < headerGap; i++) headerStr += " ";
@@ -64,7 +70,31 @@ var calendar = {
             for (i = 0; i < cellGap; i++) balanceRowStr += " ";
         }
 
-        console.log(headerStr+'\n'+dividerStr+'\n'+balanceRowStr);
+        let plotMax = Math.max.apply(null,plotData);
+        let plotMin = Math.min.apply(null,plotData);
+        const yInterval = 3000;
+        const yMax = yInterval * Math.ceil(plotMax / yInterval)
+        const yMin = yInterval * Math.floor(plotMin / yInterval)
+        const yRows = (yMax-yMin) / yInterval;
+
+        for (y = yRows; y >= 0; y--) {
+            let plotRow = "";
+
+            let rowValue = yMin + yInterval*y;
+            var labelGap = colWidth - 2 - String(rowValue).length;
+            for (i = 0; i < labelGap; i++) plotRow += " ";
+            plotRow += rowValue + " |";
+
+            for (data of plotData) {
+                // console.log(data)
+                if (data >= rowValue) plotRow += "$$$$$$    ";
+                else plotRow += "          ";
+            }
+
+            console.log(plotRow)
+        }
+
+        console.log('          '+dividerStr+'\n          '+headerStr+'\n          '+balanceRowStr);
     },
 };
 
@@ -76,15 +106,21 @@ function initCal() {
 
 function calc1Year() {
 
-    for (let i = 0; i < 12; i++) {
+    console.log('\nCalculating Income for period ' + moment().format('MMM YYYY') + " - " + moment().add(1, 'year').format('MMM YYYY') + '\n')
+    console.log('\nMonth     Income')
+    console.log('--------------------')
+
+    for (let i = 0; i < 13; i++) {
         // console.log("\n\nCal before loop:", calendar.monthSums)
         let month = moment().add(i, 'month')
-        // console.log("\nComputng month: ", month.month())
 
         const { found, val } = calendar.monthSumsBST.nearestLessThan(month.format('X'));
         // console.log("BST res: ", found, val)
         prevMonthSum = found ? calendar.monthSums[val] : calendar.init;
         // console.log("prevMonth: ", prevMonthSum)
+
+        
+        console.log(month.format("MMM")+"       "+1500)
 
         calendar.monthSumsBST.add(month.format('X'))
         calendar.monthSums[month.format('X')] = {month, totalAssets: prevMonthSum.totalAssets + 1500};
@@ -92,6 +128,11 @@ function calc1Year() {
         // console.log(calendar.monthSums[month.format('X')]);
 
         // console.log("Cal at end of loop:", calendar.monthSums)
+
+        //Slowed execution just to take a video
+        for (c = 0; c < 100E6; c++) {
+            let foo = c/(c-1)
+        }
     }
 }
 
